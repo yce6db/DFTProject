b7edd2268831fba402ef7e397bc7c02deb8d3f9d
yce6db <142350536+yce6db@users.noreply.github.com>
2026-06-12 11:02:44 -0400

    Wrote function to calculate bulk modulus and lattice constant

--- Mace_prelim/foundation.py
+++ Mace_prelim/foundation.py
@@ -9,28 +9,56 @@ from mace.calculators import mace_mp
 
 experiment = pd.read_csv("Mace_prelim/exp.csv")
 calc = mace_mp(model="medium",dispersion = False, default_dtype = "float64", device = "cpu")
-lat_values = np.linspace(3.5,4.5,11)
-volumes = []
-energies = []
-for lat in lat_values:
-    atoms = bulk("Al", "fcc", a=lat)
-    atoms.set_calculator(calc)
-    energy = atoms.get_potential_energy()
-    volume = atoms.get_volume()
-    volumes.append(volume)
-    energies.append(energy)
-eos = EquationOfState(volumes, energies, eos="murnaghan")
-v0, e0, B = eos.fit()
-a0 = (4*v0)**(1/3)
-B_GPa = B / kJ * 1e24
-atoms_bulk = bulk("Al", "fcc", a=a0)
-atoms_bulk.set_calculator(calc)
-energy_bulk = atoms_bulk.get_potential_energy()
-atom_individual = Atoms("Al", positions=[[0, 0, 0]],pbc=False)
-atom_individual.set_calculator(calc)
-energy_individual = atom_individual.get_potential_energy()
-cohesive_energy = -(energy_individual - (energy_bulk / len(atoms_bulk)))
+#lat_values = np.linspace(3.5,4.5,11)
+#olumes = []
+#energies = []
+#for lat in lat_values:
+   # atoms = bulk("Al", "fcc", a=lat)
+    #atoms.set_calculator(calc)
+   # energy = atoms.get_potential_energy()
+    #volume = atoms.get_volume()
+    #volumes.append(volume)
+   # energies.append(energy)
+#eos = EquationOfState(volumes, energies, eos="murnaghan")
+#v0, e0, B = eos.fit()
+#a0 = (4*v0)**(1/3)
+#B_GPa = B / kJ * 1e24
+#atoms_bulk = bulk("Al", "fcc", a=a0)
+#atoms_bulk.set_calculator(calc)
+#energy_bulk = atoms_bulk.get_potential_energy()
+#atom_individual = Atoms("Al", positions=[[0, 0, 0]],pbc=False)
+#atom_individual.set_calculator(calc)
+#energy_individual = atom_individual.get_potential_energy()
+#cohesive_energy = -(energy_individual - (energy_bulk / len(atoms_bulk)))
 
-print(f"Equilibrium lattice constant: {a0:.4f} Å")
-print(f"Bulk modulus: {B_GPa:.2f} GPa")
-print(f"Cohesive energy: {cohesive_energy:.4f} eV")
+#print(f"Equilibrium lattice constant: {a0:.4f} Å")
+#print(f"Bulk modulus: {B_GPa:.2f} GPa")
+#print(f"Cohesive energy: {cohesive_energy:.4f} eV")
+def calculate_bulk_and_lattice_constant(element,structure,lattice_prediction):
+    lat_values = np.linspace(lattice_prediction-0.2,lattice_prediction+0.2,15)
+    volumes = []
+    energies = []
+    for lat in lat_values:
+        atoms = bulk(element, structure, a=lat)
+        atoms.set_calculator(calc)
+        energy = atoms.get_potential_energy()
+        volume = atoms.get_volume()
+        volumes.append(volume)
+        energies.append(energy)
+    eos = EquationOfState(volumes, energies, eos="birchmurnaghan")
+    v0, e0, B = eos.fit()
+    if structure == "fcc":
+        a0 = (4*v0)**(1/3)
+    elif structure == "bcc":
+        a0 = (2*v0)**(1/3)
+    elif structure == "rocksalt":
+        a0 = (4*v0)**(1/3)
+    elif structure == "diamond":
+        a0 = (4*v0)**(1/3)
+    elif structure == "zincblende":
+        a0 = (4*v0)**(1/3)      
+   
+    B_GPa = B / kJ * 1e24
+    return a0, B_GPa
+print(calculate_bulk_and_lattice_constant("AlP","zincblende",5.458))
+    
